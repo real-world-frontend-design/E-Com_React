9867efc3017f8dcfa1a964b4a9bef29e53cac84b
Rahul-D78 <rahulparida933@gmail.com>
2021-08-15 22:01:34 +0530

    CHORE: proper folder structure created
    Signed-off-by: Rahul-D78 <rahulparida933@gmail.com>

---
+++ src/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>This is the footer code</h1>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/component/Products.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <>
+            <h1>
+                we can show products here
+            </h1>
+        </>
+    )
+}
+
+export default Products;
